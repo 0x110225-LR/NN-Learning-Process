a167e68709550884fd46c5eb8d2d918b51855410
0x_LR <54900734+CN-TEmAster@users.noreply.github.com>
2022-07-25 21:17:52 +0800

    Update Learn_Transforms_Visible.py

--- Learn_Transforms_Visible.py
+++ Learn_Transforms_Visible.py
@@ -1,3 +1,5 @@
+# transforms基础用法合集
+# b站小土堆Python深度学习课程笔记
 from PIL import Image
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
@@ -16,5 +18,23 @@ trans_norm = transforms.Normalize([1, 1, 1], [1, 1, 1])
 img_norm = trans_norm(img_tensor)
 print(img_norm[0][0][0])
 writer.add_image("Normalize", img_norm)
+
+# Resize
+print(img.size)
+trans_resize = transforms.Resize((233,233))
+
+# img PIL -> resize -> img_resize PIL
+img_resize = trans_resize(img)
+print(img_resize)
+# img_resize PIL -> ToTensor -> img_resize tensor
+img_resize = trans_totensor(img_resize)
+print(img_resize)
+writer.add_image("Resize", img_resize, 0)
+
+# Compose （另一种resize方法）
+trans_resize_2 = transforms.Resize(512)
+trans_compose = transforms.Compose([trans_resize_2, trans_totensor]) #流水线式组合操作，将多个步骤合并到一行代码执行
+img_resize_2 = trans_compose(img)
+writer.add_image("Resize-2", img_resize_2, 0)
 
 writer.close()
